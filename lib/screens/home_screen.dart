093d211ad2a6ce20ac4805035e7579f49ee8e4a3
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-17 00:35:29 +0700

    refactor UI and implement data from bloc builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/blocs/playing_movie_bloc.dart';
+import 'package:movie_app/blocs/playing_movie_state.dart';
+
+import '../widgets/card_movie_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = 'home_screen';
@@ -8,9 +13,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: BlocBuilder<PlayingMovieBloc, PlayingMovieState>(
+        builder: (context, state) {
+          if (state is PlayingMovieLoadingState) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is PlayingMovieHasDataState) {
+            return ListView.separated(
+              padding: const EdgeInsets.all(8),
+              itemBuilder: (BuildContext context, int index) {
+                return CardMovieWidget(state.movieList[index]);
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider();
+              },
+              itemCount: state.movieList.length,
+            );
+          }
+          if (state is PlayingMovieErrorState) {
+            return Center(
+              child: Text(state.message.toString()),
+            );
+          }
+          return Container();
+        },
       ),
     );
   }
