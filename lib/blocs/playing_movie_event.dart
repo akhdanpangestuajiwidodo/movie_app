80614986f04047a128cb65e5d3d7a190442eb367
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-16 23:37:02 +0700

    create playing movie event

---
+++ lib/blocs/playing_movie_event.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class PlayingMovieEvent extends Equatable {
+  const PlayingMovieEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class GetPlayingMovieEvent extends PlayingMovieEvent {}
