51f02d394f59dfe9f969faeda985d5da74351ad8
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-16 23:19:06 +0700

    create movie_remote_data_repositories to get data from API

---
+++ lib/repositories/movie_remote_data_repositories.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:movie_app/interfaces/movie_remote_data_interface.dart';
+import 'package:movie_app/models/movie_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/movie_response_model.dart';
+
+class MovieRemoteDataRepositories implements MovieRemoteDataInterface {
+  static const API_KEY = 'api_key=be7ddc7074fe58edbe5eb7645a53072d';
+  static const BASE_URL = 'https://api.themoviedb.org/3/movie';
+
+  @override
+  Future<List<MovieModel>?> getNowPlayingMovies() async {
+    final result = await http.get(Uri.parse('$BASE_URL/now_playing?$API_KEY'));
+    if (result.statusCode == 200) {
+      return MovieResponseModel.fromJson(jsonDecode(result.body)).movieList;
+    }else{
+      return null;
+    }
+  }
+}
