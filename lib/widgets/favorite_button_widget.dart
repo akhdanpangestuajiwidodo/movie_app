ca5cf4cbccff5dbc7fb22f69ba0d8489401a5131
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-17 21:06:03 +0700

    implement bloc in favorite widget

--- lib/widgets/favorite_button_widget.dart
+++ lib/widgets/favorite_button_widget.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/blocs/favorites_movie_bloc.dart';
+import 'package:movie_app/blocs/favorites_movie_event.dart';
+import 'package:movie_app/blocs/favorites_movie_state.dart';
+import 'package:movie_app/models/movie_table_model.dart';
+import 'package:movie_app/screens/favorites_screen.dart';
 
 class FavoriteButton extends StatefulWidget {
-  const FavoriteButton({Key? key}) : super(key: key);
+  final MovieTableModel movie;
+
+  const FavoriteButton(this.movie);
 
   @override
   _FavoriteButtonState createState() => _FavoriteButtonState();
@@ -12,15 +20,23 @@ class _FavoriteButtonState extends State<FavoriteButton> {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        isFavorite ? Icons.favorite : Icons.favorite_border,
-        color: Colors.red,
-      ),
-      onPressed: () {
-        setState(() {
-          isFavorite = !isFavorite;
-        });
+    return BlocBuilder<FavoritesMovieBloc, FavoritesMovieState>(
+      builder: (context, state) {
+        return IconButton(
+          icon: Icon(
+            isFavorite ? Icons.favorite : Icons.favorite_border,
+            color: Colors.red,
+          ),
+          onPressed: () {
+            context
+                .read<FavoritesMovieBloc>()
+                .add(AddFavoritesMovieEvent(widget.movie));
+            setState(() {
+              isFavorite = !isFavorite;
+            });
+            Navigator.pushNamed(context, FavoriteScreen.routeName);
+          },
+        );
       },
     );
   }
