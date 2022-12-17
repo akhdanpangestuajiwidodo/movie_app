41e1038827002ff6a7e8fcbdc8d82fc012056ba8
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-17 10:53:58 +0700

    initial detail movie that consume data from state

---
+++ lib/screens/detail_movie_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/blocs/detail_movie_bloc.dart';
+import 'package:movie_app/blocs/detail_movie_event.dart';
+import 'package:movie_app/blocs/detail_movie_state.dart';
+
+import '../widgets/favorite_button_widget.dart';
+
+class DetailMovieScreen extends StatefulWidget {
+  static const routeName = 'detail_movie_screen';
+  final int id;
+
+  DetailMovieScreen({required this.id});
+
+  @override
+  State<DetailMovieScreen> createState() => _DetailMovieScreenState();
+}
+
+class _DetailMovieScreenState extends State<DetailMovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<DetailMovieBloc>().add(GetDetailMovieEvent(widget.id));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<DetailMovieBloc, DetailMovieState>(
+          builder: (context, state) {
+        if (state is DetailMovieLoadingState) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (state is DetailMovieHasDataState) {
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    Image.network(
+                        'https://image.tmdb.org/t/p/w500${state.movie.posterPath}'),
+                    SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_back,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                            const FavoriteButton(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 16.0),
+                  child: Text(
+                    state.movie.originalTitle,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 30.0,
+                      fontFamily: 'Staatliches',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    state.movie.overview,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Oxygen',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        if (state is DetailMovieErrorState) {
+          return Center(
+            child: Text(state.message.toString()),
+          );
+        }
+        return Container();
+      }),
+    );
+  }
+}
