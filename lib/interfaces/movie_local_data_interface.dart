d8281e2f5d30bd2fefa256d7674dd2d5779d6486
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-17 22:13:08 +0700

    add delete movie method

--- lib/interfaces/movie_local_data_interface.dart
+++ lib/interfaces/movie_local_data_interface.dart
@@ -3,4 +3,6 @@ import 'package:movie_app/models/movie_table_model.dart';
 abstract class MovieLocalDataInterface{
   Future<void> addFavoriteMovie(MovieTableModel movie);
   Future<List<MovieTableModel>?> getFavoriteMovie(String username);
+  Future<bool> getByTitleAndUsername(MovieTableModel movie);
+  Future<int> deleteFavoriteMovie(MovieTableModel movie);
 }
