11a536402370ba7ce3bfb31c33164b7098467823
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-20 17:54:27 +0700

    create auth state

---
+++ lib/blocs/auth_state.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+class AuthState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthLoadingState extends AuthState {}
+
+class AuthenticatedState extends AuthState {}
+
+class UnAuthenticatedState extends AuthState {}
+
+class AuthErrorState extends AuthState {
+  final String error;
+
+  AuthErrorState(this.error);
+
+  @override
+  List<Object?> get props => [error];
+}
