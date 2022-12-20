1d46cfb230d6335b09296fc48ffe5d2a500e57c6
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-20 19:15:31 +0700

    add user data from auth in homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -28,8 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
     _scrollController.addListener(() {
       if (_scrollController.position.pixels ==
           _scrollController.position.maxScrollExtent) {
-        context.read<PlayingMovieBloc>().add(
-            GetPlayingMovieEvent());
+        context.read<PlayingMovieBloc>().add(GetPlayingMovieEvent());
       }
     });
   }
@@ -42,29 +42,54 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
     return Scaffold(
       drawer: Drawer(
-        child: Column(
-          children: [
-            const UserAccountsDrawerHeader(
-              accountName: Text('Movie App'),
-              accountEmail: Text('akhdan'),
-            ),
-            ListTile(
-              leading: Icon(Icons.movie),
-              title: Text('Movies'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.save),
-              title: Text('Favorites'),
-              onTap: () {
-                Navigator.pushNamed(context, FavoriteScreen.routeName);
-              },
-            ),
-          ],
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ConstrainedBox(
+                constraints: const BoxConstraints(minWidth: double.infinity),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage('${user.photoURL}'),
+                        ),
+                        const SizedBox(height: 10),
+                        Text('${user.displayName}'),
+                        const SizedBox(height: 4),
+                        Text('${user.email}'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.movie),
+                title: Text('Movies'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.save),
+                title: Text('Favorites'),
+                onTap: () {
+                  Navigator.pushNamed(context, FavoriteScreen.routeName);
+                },
+              ),
+            ],
+          ),
         ),
       ),
       appBar: AppBar(
