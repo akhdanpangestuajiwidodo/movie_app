95da27ddfe1ce9be4c55d0496756a3a9cba5991f
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-20 18:55:40 +0700

    create sign in screen

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,169 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/blocs/auth_bloc.dart';
+import 'package:movie_app/blocs/auth_event.dart';
+import 'package:movie_app/blocs/auth_state.dart';
+import 'package:movie_app/screens/home_screen.dart';
+import 'package:movie_app/screens/sign_up_screen.dart';
+
+class SignInScreen extends StatefulWidget {
+  static const routeName = 'sign_in_screen';
+
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("SignIn"),
+      ),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthenticatedState) {
+            Navigator.pushNamed(context, HomeScreen.routeName);
+          }
+          if (state is AuthErrorState) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(state.error)));
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoadingState) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is UnAuthenticatedState) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: SingleChildScrollView(
+                  reverse: true,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                          fontSize: 38,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 18,
+                      ),
+                      Center(
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                controller: _emailController,
+                                decoration: const InputDecoration(
+                                  hintText: "Email",
+                                  border: OutlineInputBorder(),
+                                ),
+                                autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                                validator: (value) {
+                                  return value != null &&
+                                      !EmailValidator.validate(value)
+                                      ? 'Enter valid email'
+                                      : null;
+                                },
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                controller: _passwordController,
+                                decoration: const InputDecoration(
+                                  hintText: "Password",
+                                  border: OutlineInputBorder(),
+                                ),
+                                autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                                validator: (value) {
+                                  return value != null && value.length < 6
+                                      ? "Enter min. 6 characters"
+                                      : null;
+                                },
+                              ),
+                              const SizedBox(
+                                height: 12,
+                              ),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.7,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    _authenticatedWithEmailAndPassword(context);
+                                  },
+                                  child: const Text('Sign In'),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _autenticateWithGoogle(context);
+                        },
+                        icon: Image.network(
+                          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png",
+                          height: 30,
+                          width: 30,
+                        ),
+                      ),
+                      const Text("Don't have an account?"),
+                      OutlinedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, SignUpScreen.routeName);
+                        },
+                        child: const Text("Sign Un"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }
+          if (state is AuthErrorState) {
+            return Center(
+              child: Text(state.error),
+            );
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+
+  void _authenticatedWithEmailAndPassword(context) {
+    if (_formKey.currentState!.validate()) {
+      BlocProvider.of<AuthBloc>(context)
+          .add(SignInEvent(_emailController.text, _passwordController.text));
+    }
+  }
+
+  void _autenticateWithGoogle(context) {
+    BlocProvider.of<AuthBloc>(context).add(GoogleSignInEvent());
+  }
+}
