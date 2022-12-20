e4310d07b2677e29a233ee6486d27877d056c909
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-20 17:38:55 +0700

    refactor to auth interface

--- lib/interfaces/auth_interface.dart
+++ lib/interfaces/auth_interface.dart
@@ -1,4 +1,4 @@
-abstract class AuthDataInterface{
+abstract class AuthInterface{
   Future<void> signUp(String email, String password);
   Future<void> signIn(String email, String password);
   Future<void> signOut();
