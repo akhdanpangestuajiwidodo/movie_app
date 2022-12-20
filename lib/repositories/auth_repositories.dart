a8fe962e05b0e571f519b4a3d6c726e13c5c1fbc
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-20 17:45:44 +0700

    ralat: create signUp method

--- lib/repositories/auth_repositories.dart
+++ lib/repositories/auth_repositories.dart
@@ -5,19 +5,7 @@ class AuthRepositories implements AuthInterface {
   final _firebaseAuth = FirebaseAuth.instance;
 
   @override
-  Future<void> signIn(String email, String password) async {
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-    }on FirebaseAuthException catch (e){
-      if(e.code == 'weak-password') {
-        throw Exception('The password is weak');
-      }else if (e.code == 'email-already-in-use') {
-        throw Exception('The email already exists');
-        }
-    } catch (e){
-      throw Exception(e.toString());
-    }
-  }
+  Future<void> signIn(String email, String password) async {}
 
   @override
   Future<void> signInWithGoogle() async {}
@@ -26,6 +14,18 @@ class AuthRepositories implements AuthInterface {
   Future<void> signOut() async {}
 
   @override
-  Future<void> signUp(String email, String password) async{}
-
+  Future<void> signUp(String email, String password) async {
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw Exception('The password is weak');
+      } else if (e.code == 'email-already-in-use') {
+        throw Exception('The email already exists');
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
 }
