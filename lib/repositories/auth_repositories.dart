3e0865c737965cfbb51962aefca1727471a45373
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-12-20 17:44:55 +0700

    create signIn method

---
+++ lib/repositories/auth_repositories.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:movie_app/interfaces/auth_interface.dart';
+
+class AuthRepositories implements AuthInterface {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Future<void> signIn(String email, String password) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+    }on FirebaseAuthException catch (e){
+      if(e.code == 'weak-password') {
+        throw Exception('The password is weak');
+      }else if (e.code == 'email-already-in-use') {
+        throw Exception('The email already exists');
+        }
+    } catch (e){
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future<void> signInWithGoogle() async {}
+
+  @override
+  Future<void> signOut() async {}
+
+  @override
+  Future<void> signUp(String email, String password) async{}
+
+}
